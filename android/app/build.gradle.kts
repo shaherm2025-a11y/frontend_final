6b6c9552b2a43c2da11ee79c9fcd9eac86156cca
Mohammed Shaher <shaherm2025@gmail.com>
2025-08-31 06:26:49 +0300

    Update compileSdk and targetSdk to 35

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -8,7 +8,7 @@ plugins {
 android {
     ndkVersion = "27.0.12077973" // or whatever version you installed 
     namespace = "com.example.plant_diagnosis"
-    compileSdk = flutter.compileSdkVersion
+    compileSdk = 35
     
 
     compileOptions {
@@ -26,7 +26,7 @@ android {
         // You can update the following values to match your application needs.
         // For more information, see: https://flutter.dev/to/review-gradle-config.
         minSdk = flutter.minSdkVersion
-        targetSdk = flutter.targetSdkVersion
+        targetSdk = 35
         versionCode = flutter.versionCode
         versionName = flutter.versionName
 		multiDexEnabled = (true)
