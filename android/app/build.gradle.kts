3bf9371b43d0c6a4d0ac69279686c2a4127c96dd
Mohammed Shaher <shaherm2025@gmail.com>
2025-09-01 05:31:33 +0300

    alter gardel

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -1,28 +1,26 @@
 plugins {
-    id "com.android.application"
-    id "org.jetbrains.kotlin.android"
-    id "dev.flutter.flutter-gradle-plugin"
+    id("com.android.application")
+    id("org.jetbrains.kotlin.android")
+    id("dev.flutter.flutter-gradle-plugin")
 }
 
 android {
-    namespace "com.example.plant_diagnosis"
-    compileSdk 35
-
-    ndkVersion "27.0.12077973"
+    namespace = "com.example.plant_diagnosis"
+    compileSdk = 35
+    ndkVersion = "27.0.12077973"
 
     defaultConfig {
-        applicationId "com.example.plant_diagnosis"
-        minSdk 21
-        targetSdk 35
-        versionCode 1
-        versionName "1.0"
-
-        multiDexEnabled true
+        applicationId = "com.example.plant_diagnosis"
+        minSdk = 21
+        targetSdk = 35
+        versionCode = 1
+        versionName = "1.0"
+        multiDexEnabled = true
     }
 
     compileOptions {
-        sourceCompatibility JavaVersion.VERSION_11
-        targetCompatibility JavaVersion.VERSION_11
+        sourceCompatibility = JavaVersion.VERSION_11
+        targetCompatibility = JavaVersion.VERSION_11
     }
 
     kotlinOptions {
@@ -30,20 +28,19 @@ android {
     }
 
     buildTypes {
-        release {
-            // مؤقتاً استخدم debug key عشان يشتغل
-            signingConfig signingConfigs.debug
-            minifyEnabled false
-            shrinkResources false
+        getByName("release") {
+            signingConfig = signingConfigs.getByName("debug")
+            isMinifyEnabled = false
+            isShrinkResources = false
         }
     }
 }
 
 flutter {
-    source "../.."
+    source = "../.."
 }
 
 dependencies {
-    implementation "org.jetbrains.kotlin:kotlin-stdlib-jdk7:1.9.22"
-    implementation "androidx.multidex:multidex:2.0.1"
+    implementation("org.jetbrains.kotlin:kotlin-stdlib-jdk7:1.9.22")
+    implementation("androidx.multidex:multidex:2.0.1")
 }
